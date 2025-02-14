6e5e506858c68cd14f8e4a011c0b92ffba9debff
mayur wagh <mswagh98@gmail.com>
2025-02-14 16:59:05 +0530

    Implements user logout functionality
    Adds a logout endpoint that clears user session cookies and returns a success message.
    
    Enhances the profile page with a logout button that triggers the logout API call, providing user feedback upon successful logout.
    
    Improves user experience by redirecting to the login page after logout

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,31 @@
+"use client";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
 const Profile = () => {
-  return <div>this is a profile page</div>;
+  const router = useRouter();
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logout successfully");
+      router.push("/login");
+    } catch (error) {
+      console.log("error:", error);
+    }
+  };
+
+  return (
+    <div className="w-50% flex flex-col justify-center items-center mt-[10%]">
+      this is a profile page
+      <button
+        className="bg-blue-500 px-3 py-2 rounded-md mt-2"
+        onClick={logout}
+      >
+        Logout
+      </button>
+    </div>
+  );
 };
 
 export default Profile;
